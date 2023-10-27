448e057493dffb5fdb927d9fddeaa70cdfe70701
roc-11 <>
2023-10-27 08:55:40 +0000

    Create get_rand_word function to get random word from google sheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,7 +13,16 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSREAD_CLIENT.open('hangman')
 
-#test the connection to Google Sheets/API Creds
-words = SHEET.worksheet('words')
-data = words.get_all_values()
-print(data)
+print("Welcome to Hangman")
+print("______________________________")
+
+def get_rand_word():
+    """
+    Get random word from Google Sheets words list
+    """
+    words_sheet = SHEET.worksheet('words')
+    words_list = words_sheet.get_all_values()
+    word = random.choice(words_list)
+    return word
+
+get_rand_word()
